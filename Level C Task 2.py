03e95b58c40607de37388dcfbdf212b75118ea6d
psgrussa <andrvasilenko2011@gmail.com>
2024-09-24 11:05:52 +0700

    Update Level C Task 2.py

--- Level C Task 2.py
+++ Level C Task 2.py
@@ -8,3 +8,5 @@ simvolov_na2stanise = simvolov_nastanise*2
 print(simvolov_na2stanise, 'Символов на двух страницах')
 informasiya = simvolov_na2stanise*5
 print(informasiya, 'Информации на двух страницах (биты)')
+else:
+    print('ошибка ввода')
