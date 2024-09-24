bd6cd79588347b120f9cdf3f798c43f6678b3a21
psgrussa <andrvasilenko2011@gmail.com>
2024-09-24 10:41:23 +0700

    Второе задание уровня С

---
+++ Level C Task 2.py
@@ -0,0 +1,10 @@
+print('Сколько у вас строк на странице?')
+stroki = int(input())
+print('Сколько у вас символов на одной строке?')
+simvoli = int(input())
+simvolov_nastanise = stroki*simvoli
+print(simvolov_nastanise, 'Символов на одной странице')
+simvolov_na2stanise = simvolov_nastanise*2
+print(simvolov_na2stanise, 'Символов на двух страницах')
+informasiya = simvolov_na2stanise*5
+print(informasiya, 'Информации на двух страницах (биты)')
