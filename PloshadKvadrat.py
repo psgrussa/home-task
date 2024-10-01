93c6625b785853c47264784698f69161ff62830c
psgrussa <andrvasilenko2011@gmail.com>
2024-10-01 11:04:47 +0700

    Update PloshadKvadrat.py

--- PloshadKvadrat.py
+++ PloshadKvadrat.py
@@ -2,8 +2,8 @@ print('введите длину фигуры')
 dlina = int(input())
 print('введите ширину фигуры')
 shirina = int(input())
-print('площадь какой фигуры вы хотите вычислить?')
-figura = input()
-if figura == 'квадрат':
-    ploshad_kvadrat = dlina*shirina
-    print(ploshad_kvadrat)
+if shirina != dlina:
+    print('это не квадрат!')
+else:
+ploshad_kvadrat = dlina*shirina
+print(ploshad_kvadrat)
